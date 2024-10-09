8cbf33d1a0298b77e6e8571683d0fcaa07a28b8a
DERVISEMREDEMIR-STUD <g00427391@atu.ie>
2024-10-09 16:36:49 +0100

    prompts added

---
+++ src/main/java/org/example/StudentApp.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class StudentApp {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter a student name: \n");
+        String studentName = sc.nextLine();
+
+        System.out.println("Please enter the student email: \n");
+        String emailName = sc.nextLine();
+
+        System.out.println("Please enter the student course: \n");
+        String courseName = sc.nextLine();
+
+    }
+}
